6471b9d12ee51120830186f5e8b9a4d90afc12d9
Thaw Zin Lynn <thawzinbright@gmail.com>
2026-03-03 07:50:04 +0000

    Day 1 and Day 2

---
+++ Day1_thawZinLynn/10DaysOfPython/day_2/variables.py
@@ -0,0 +1,94 @@
+#Day 2: 10 Days of python programming
+#No3
+firstName = "Thaw"
+
+#No4
+lastName = "Lynn"
+
+#No5
+fullName = firstName + " Zin " + lastName
+
+#No6
+country = "Myanmar"
+
+#No7
+city = "Yangon"
+
+#No8
+age = 20
+
+#No9
+year = 2026
+
+#No10
+is_married = False
+
+#No11
+is_true = True
+
+#No12
+is_light_on = True
+
+#No13
+a, b, c = 1, "HEllO", 3.14
+
+#Exercises Level 2
+#No1
+print(type(firstName))  
+print(type(lastName))   
+print(type(fullName))     
+print(type(country))     
+print(type(city))       
+print(type(age))         
+print(type(year))       
+print(type(is_married))  
+print(type(is_true))     
+print(type(is_light_on))
+print(type(a))            
+print(type(b))            
+print(type(c))           
+
+#No2
+print(len(firstName))
+
+#No3
+FIRST = print(len(firstName))
+LAST = print(len(lastName))
+print("Compare ", FIRST, " and ", LAST)
+
+#No4
+num_one = 5
+num_two = 4
+#i
+variable_total = num_one + num_two
+#ii
+variable_diff = num_one - num_two
+#iii
+variable_product = num_one * num_two
+#iv
+variable_division = num_one / num_two
+#v
+variable_remainder = num_one % num_two
+#vi
+variable_exp = num_one ** num_two
+#vii
+variable_floor_division = num_one // num_two
+
+#No5
+radius = 30
+#i
+area_of_circle = 3.14 * radius ** 2
+#ii
+circum_of_circle = 2 * 3.14 * radius
+#iii
+user_radius = int(input("Enter the radius of a circle: "))
+print("Area of circle: ", 3.14 * user_radius ** 2)
+
+#No6
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+new_country = input("Enter your country: ")
+new_age = int(input("Enter your age: "))
+
+#No7
+print(help('keywords'))
